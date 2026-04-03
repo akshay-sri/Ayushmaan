de89ad3146c06bd08e9e667de4721ebf8ff559d7
akshay-sri <sriakshay2611999@gmail.com>
2026-04-03 20:30:23 +0530

    Registered the services with eureka

--- AmbulanceService/src/main/java/com/ayushmaan/ambulance/AmbulanceServiceApplication.java
+++ AmbulanceService/src/main/java/com/ayushmaan/ambulance/AmbulanceServiceApplication.java
@@ -2,8 +2,10 @@ package com.ayushmaan.ambulance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AmbulanceServiceApplication {
 
 	public static void main(String[] args) {
