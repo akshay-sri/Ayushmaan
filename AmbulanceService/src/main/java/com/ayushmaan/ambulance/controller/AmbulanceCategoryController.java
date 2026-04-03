e2c6afc3985f1fe7c9ce0ef5d8c88a9dc2ff6bc7
akshay-sri <sriakshay2611999@gmail.com>
2026-04-03 20:30:23 +0530

    Registered the services with eureka

--- AmbulanceService/src/main/java/com/ayushmaan/ambulance/controller/AmbulanceCategoryController.java
+++ AmbulanceService/src/main/java/com/ayushmaan/ambulance/controller/AmbulanceCategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ambulance-category")
+@RequestMapping("/ambulance")
 @Tag(name = "Ambulance-category Apis", description = "Operations related to ambulance-category")
 public class AmbulanceCategoryController {
 
@@ -21,26 +21,26 @@ public class AmbulanceCategoryController {
         this.ambulanceCategoryService = ambulanceCategoryService;
     }
 
-    @PostMapping("/add")
+    @PostMapping("/add-category")
     @Operation(summary = "Add a new ambulance category", description = "Adds new ambulance category in the database")
     public ResponseEntity<String> addAmbulanceCategory(@RequestBody AmbulanceCategoryDto ambulanceCategoryDto) {
         return new ResponseEntity<>(ambulanceCategoryService.add(ambulanceCategoryDto), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/delete/{categoryName}")
+    @DeleteMapping("/delete-category/{categoryName}")
     @Operation(summary = "Delete an ambulance category", description = "Deletes an ambulance category from the database")
     public ResponseEntity<String> deleteAmbulanceCategory(@PathVariable String categoryName) {
         return new ResponseEntity<>(ambulanceCategoryService.delete(categoryName), HttpStatus.CREATED);
     }
 
-    @PatchMapping("/update/{categoryName}")
+    @PatchMapping("/update-category/{categoryName}")
     @Operation(summary = "Update an ambulance category", description = "Update an ambulance category in the database")
     public ResponseEntity<String> updateAmbulanceCategory(@RequestBody AmbulanceCategoryDto ambulanceCategoryDto,
                                                         @PathVariable String categoryName) {
         return new ResponseEntity<>(ambulanceCategoryService.update(ambulanceCategoryDto, categoryName), HttpStatus.CREATED);
     }
 
-    @GetMapping("/get")
+    @GetMapping("/get-categories")
     public ResponseEntity<List<AmbulanceCategoryDto>> getAllAmbulanceCategories(){
         return new ResponseEntity<>(ambulanceCategoryService.get(), HttpStatus.OK);
     }
