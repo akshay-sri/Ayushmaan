b8b92ecbb0077edf788949824e32d3b62c0dadee
akshay-sri <sriakshay2611999@gmail.com>
2026-04-05 21:25:54 +0530

    JWT Validation completed in APIGatewayService

---
+++ APIGatewayService/src/main/java/com/ayushmaan/APIGatewayService/RouteValidator.java
@@ -0,0 +1,18 @@
+package com.ayushmaan.APIGatewayService;
+
+import java.util.List;
+
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/ayushmaan/auth/login",
+            "/ayushmaan/auth/register"
+    );
+
+    public static boolean isSecured(String path) {
+
+        return openApiEndpoints
+                .stream()
+                .noneMatch(path::startsWith);
+    }
+}
